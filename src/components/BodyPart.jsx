f907ba3e5ca397d105e90ce0b223b120161b0550
Dima Yanovskji <criska7@gmail.com>
2023-01-17 22:34:29 +0200

    Maste
    add BodyPart

--- src/components/BodyPart.jsx
+++ src/components/BodyPart.jsx
@@ -23,12 +23,16 @@ const BodyPart = ({ item, setBodyPart, bodyPart }) => {
         cursor: "pointer",
         gap: "47px",
       }}
+      onClick={() => {
+        setBodyPart(item);
+      }}
     >
       <img
         src={Icon}
         alt="dumbbell"
-        styles={{ width: "40px", height: "40px" }}
+        style={{ width: "40px", height: "40px" }}
       />
+      <Typography fontSize='24px' fontWeight='bold' color='#3A1212' textTransform='capitalize'>{item}</Typography>
     </Stack>
   );
 };
