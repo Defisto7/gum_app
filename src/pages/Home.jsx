19a641ee678255055d3d44550e6a1ac3e4a529a2
Dima Yanovskji <criska7@gmail.com>
2023-01-17 10:43:44 +0200

    Master
    add horizontalScrolbar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,28 @@
-import { useState } from 'react';
+import { useState } from "react";
 import { Box } from "@mui/material";
-import HeroBanner from '../components/HeroBanner';
-import SearchExercises from '../components/SearchExercises';
-import Exercises from '../components/Exercises';
+import HeroBanner from "../components/HeroBanner";
+import SearchExercises from "../components/SearchExercises";
+import Exercises from "../components/Exercises";
 
 const Home = () => {
+  const [bodyPart, setBodyPart] = useState("all");
+  const [exercise, setExercise] = useState([]);
+
   return (
     <Box>
-        <HeroBanner />
-        <SearchExercises />
-        <Exercises />
+      <HeroBanner />
+      <SearchExercises
+        setExercise={setExercise}
+        bodyPart={bodyPart}
+        setBodyPart={setBodyPart}
+      />
+      <Exercises
+        setExercise={setExercise}
+        bodyPart={bodyPart}
+        setBodyPart={setBodyPart}
+      />
     </Box>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
